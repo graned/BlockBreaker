e3d6cfca5ccd483210232b7e516238973c1279b1
unknown <j.anaya.villagrana@0262-VILLAGRANA.CALHEERS.LOCAL>
2015-03-01 11:16:39 -0800

    Block Breaker Unity

---
+++ Assets/Scripts/LooseColider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LooseColider : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Debug.Log("Trigger");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		Debug.Log("Collision");
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer musicPlayer;
+
+	void Awake(){
+		Debug.Log ("Awake: " + GetInstanceID ());
+		if (musicPlayer != null) {
+			Destroy (gameObject);
+		} else {
+			musicPlayer = this;
+		}
+		GameObject.DontDestroyOnLoad (gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start: " + GetInstanceID ());
+		//gameObject.GetComponent ("Audio Source");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
