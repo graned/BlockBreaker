07f504c3648c2924ac08ab4d56f6707c06702e56
Graned <Eduardo@Chobby.local>
2015-03-01 20:32:11 -0800

    Added a paddle, physics, corrected bricks sizes.

--- Assets/Scripts/LooseColider.cs
+++ Assets/Scripts/LooseColider.cs
@@ -3,10 +3,21 @@ using System.Collections;
 
 public class LooseColider : MonoBehaviour {
 
+	/*
+	 * ESTE METODO CAPTURA EL EVENTO TRIGGER CUANDO SUCEDE UNA COLISION DE LA BOLA CON EL GAME OBJECT LLAMADO LOOSE COLIDER, 
+	 * Y MANDA LLAMAR LA PANTALLA DE WIN SCREEN.
+	 */
 	void OnTriggerEnter2D(Collider2D collider){
 		Debug.Log("Trigger");
+
+		new LevelManager ().LoadLevel ("Win Screen");
+
 	}
 
+	/*
+	 * ESTE METODO CAPTURA EL EVENTO COLLISION CUANDO SUCEDE UNA COLISION DE LA BOLA CON EL GAME OBJECT LLAMADO LOOSE COLIDER, 
+	 * Y MANDA LLAMAR LA PANTALLA DE WIN SCREEN.
+	 */
 	void OnCollisionEnter2D(Collision2D collision){
 		Debug.Log("Collision");
 	}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+	Vector3 vector;
+	// Use this for initialization
+	void Start () {
+		vector = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+		vector.x = Mathf.Clamp(mousePosInBlocks,0.5f,15.5f);
+		this.transform.position = vector;
+	}
+}
