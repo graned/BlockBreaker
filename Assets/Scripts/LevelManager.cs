7b908c730ae6c6a6c88852f1bb6edef5de5f8f7a
unknown <j.anaya.villagrana@0262-VILLAGRANA.CALHEERS.LOCAL>
2015-03-20 17:41:14 -0700

    added new instruction page and the logic for auto-loading levels is pending

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,12 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class LevelManager : MonoBehaviour {
+	/*
+	private event LevelLoadedEventHandler levelLoadedEventHandler;
+
+	private delegate void LevelLoadedEventHandler(object sender, LevelLoadedEventArgs levelLoadedEventArgs);
+
+	private class LevelLoadedEventArgs : EventArgs{
+		private String levelName;
+
+		public String LevelName{
+			set{
+				this.levelName= value;
+			}
+			get{
+				return levelName;
+			}
+		}
+
+	};
+
+	private LevelLoadedEventArgs levelEventArgs;
+	*/
 	void Start(){
 		Brick.breakableCount = GameObject.FindGameObjectsWithTag ("Breakable").Length;
+		//levelEventArgs = new LevelLoadedEventArgs ();
 	}
-
 	public void LoadLevel(string name){
+		/*
+		levelEventArgs.LevelName = name;
+		levelLoadedEventHandler (this, levelEventArgs);
+		*/
+
 		Application.LoadLevel (name);
 		if ("Level_01".Equals (name)) {
 			Screen.showCursor = false;
@@ -21,14 +48,26 @@ public class LevelManager : MonoBehaviour {
 	}
 
 	public void loadNextLevel(){
+
 		Application.LoadLevel (Application.loadedLevel + 1);
-		if (Application.loadedLevel == 4) {
-			Screen.showCursor = true;
+		//UPDATE!
+		Debug.Log (Application.loadedLevelName);
+		if ("Instruction Screen".Equals (Application.loadedLevelName)) {
+			//Debug.Log("PSSSSSSSSSSSSSSS!");
+			automaticLevelLoad(2000);
 		}
+		/*if (Application.loadedLevel == 4) {
+			Screen.showCursor = true;
+		}*/
 	}
 	public void brickDestroyedMessage(){
 		if(Brick.breakableCount <= 0){
 			loadNextLevel();
 		}
+	}
+
+	public void automaticLevelLoad(int seconds){
+		loadNextLevel ();
+		System.Threading.Thread.Sleep (seconds);
 	}
 }
