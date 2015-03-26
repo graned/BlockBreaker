8aa2825a17a1ffa20f2de4215cf62fc9e2a90267
unknown <j.anaya.villagrana@0262-VILLAGRANA.CALHEERS.LOCAL>
2015-03-25 18:13:21 -0700

    fixed the autoloading issue, added a thread that will decrease a variable of level manager in parallel. TODO: NEED TO RESET THE CURRENT LEVEL COUNTER WHEN THE PLAYER LOSES IN THE GAME!

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,40 +3,83 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
-	private int currentLevel = 1;
+	private static int currentLevel = 1;
 	public Text levelTextValue;
+	private int seconds;
 	private bool needWait = true;
+
+	private class TimerThread{
+		private LevelManager levelManager;
+		private string levelToLoad;
+
+		public TimerThread(LevelManager levelManager, string levelToLoad){
+			this.levelManager = levelManager;
+			this.levelToLoad = levelToLoad;
+		}
+
+		public void DoWork(){
+			Debug.Log ("wait starts!");
+			while (levelManager.seconds > 0) {
+				Debug.Log ("seconds: "+levelManager.seconds);
+				System.Threading.Thread.Sleep(1000);
+				levelManager.seconds--;
+			}
+			Debug.Log ("wait finished!");
+			//callOtherLevel ();
+		}
+		public void callOtherLevel(){
+			Debug.Log ("callOtherLevel method!");
+			//Application.LoadLevel (levelToLoad);
+			levelManager.LoadLevel (levelToLoad);
+		}
+	}
 
 	void Start(){
 		Brick.breakableCount = GameObject.FindGameObjectsWithTag ("Breakable").Length;
 	}
 	public void LoadLevel(string name){
+		Debug.Log ("Trying to load: " + name);
 		Application.LoadLevel (name);
-
 	}
 
 	public void Update(){
 		//Debug.Log ("Is loading Level?: " + Application.isLoadingLevel);
-		levelPresets (Application.loadedLevelName);
-
+		//levelPresets (Application.loadedLevelName);
+		if (Application.loadedLevel == 1) {
+			if(seconds == 0){
+				LoadLevel("Level_"+currentLevel);
+			}
+		}
 	}
-	private void levelPresets(string levelName){
-		Debug.Log(levelName + " : "+Application.loadedLevel);
 
-		if(Application.loadedLevel == 1){
+	public void OnLevelWasLoaded(int level){
+
+		Debug.Log (level + " level loaded");
+		if (level == 1) {
+			Debug.Log ("introcution level loaded");
 			needWait = true;
-			levelTextValue.text = currentLevel.ToString();
-			LoadLevel("Level_"+currentLevel);
-		}else if(("Level_"+currentLevel).Equals(levelName)){
+			levelTextValue.text = currentLevel.ToString ();
+			seconds = 5;
+			TimerThread tt = new TimerThread(this,"Level_"+currentLevel);
+			System.Threading.Thread timerThread = new System.Threading.Thread(tt.DoWork);
+			timerThread.Start();
+			//System.Threading.Thread.Sleep(3000);
+			//CALL THREAD THAT WILL CALL THE LOAD LEVEL METHOD
+			//LoadLevel ("Level_" + currentLevel);
+		}
+	}
+
+	private void levelPresets(string levelName){
+		Debug.Log ("Level_" + currentLevel + " = " + levelName);
+		if(("Level_"+currentLevel).Equals(levelName)){
 			if(needWait){
-				//Debug.Log("ENTERED!");
-				System.Threading.Thread.Sleep(3000);
+				System.Threading.Thread.Sleep(1000);
 				needWait = false;
 			}
 		}
-		Debug.Log ("Level_" + currentLevel +" = "+levelName);
 	}
-	//SINTAX EQUALS IN JAVA TO  <T EXTENDS GAMEOBJECT>
+
+	/*//SINTAX EQUALS IN JAVA TO  <T EXTENDS GAMEOBJECT>
 	private T getComponentByName<T>(string componentName) where T:Component{
 		Debug.Log("IN METHOD");
 		Debug.Log("COMPONENT_NAME: "+componentName);
@@ -52,7 +95,7 @@ public class LevelManager : MonoBehaviour {
 		//ITEM NOT FOUND
 		return null;
 	}
-
+	*/
 	public void QuitRequest(){
 		Debug.Log ("Quit requested");
 		Application.Quit ();
@@ -66,9 +109,8 @@ public class LevelManager : MonoBehaviour {
 	}
 	public void brickDestroyedMessage(){
 		if(Brick.breakableCount <= 0){
-
 			Debug.Log("current level(before): "+currentLevel);
-			++currentLevel;
+			currentLevel += 1;
 			Debug.Log("current level(after): "+currentLevel);
 
 			LoadLevel("Level Introduction");
